9783b98d65b237d9217e7cda216736d2b35a045c
Nyso97 <justin0907@naver.com>
2026-03-03 17:25:08 +0900

    메인, 로컬, 온라인 씬 분리

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
+// 현재 임시 스크립트
+/*
+    TODO?
+    - 다른 코드의 UI 코드를 이 스크립트로 이동
+    - InputManager의 inputavailable 구독하여 playerButtonUI 활성화/비활성화
+    - 일시정지?
+*/
+
 public class CanvasManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+    }
+}
